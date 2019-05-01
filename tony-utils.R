69257c64f16f4845db517f621276ca7a49605dea
Tony Fischetti <tony.fischetti@gmail.com>
2019-05-01 13:11:51 -0400

    better pivot function and [set]?cleannames

--- tony-utils.R
+++ tony-utils.R
@@ -44,10 +44,15 @@
   delcols(DT, these)
 }
 
-.env$getxtab <- function(DT, theby){
-  tmp <- DT[, .N, theby][order(-N)]
-  thetotal <- tmp[, sum(N)]
-  tmp[, percent:=round(N/thetotal*100, 2)]
+pivot <- function(DT, theby, theexp, cutoff=0){
+  inexp <- substitute(theexp)
+  strexp <- as.character(deparse(inexp))
+  template <- sprintf("DT[, .(val=%s), theby][order(-val)]", strexp)
+  tmp <- eval(parse(text=template))
+  thetotal <- tmp[, sum(val)]
+  tmp[, percent:=round(val/thetotal*100, 2)]
+  tmp[percent<=cutoff, (theby):="OTHER"]
+  tmp <- tmp[, .(val=sum(val), percent=sum(percent)), theby]
   tmp2 <- tmp[1,]
   tmp2[1,1] <- "TOTAL"
   tmp2[1,2] <- thetotal
@@ -55,17 +60,15 @@
   rbind(tmp, tmp2)
 }
 
-.env$getxtab2 <- function(DT, theby, cutoff){
-  tmp <- DT[, .N, theby][order(-N)]
-  thetotal <- tmp[, sum(N)]
-  tmp[, percent:=round(N/thetotal*100, 2)]
-  tmp[percent<=cutoff, (theby):="OTHER"]
-  tmp <- tmp[, .(N=sum(N), percent=sum(percent)), theby]
-  tmp2 <- tmp[1,]
-  tmp2[1,1] <- "TOTAL"
-  tmp2[1,2] <- thetotal
-  tmp2[1,3] <- 100
-  rbind(tmp, tmp2)
+cleannames <- function(dat){
+  thenames <- names(dat)
+  nospace <- stringr::str_replace_all(thenames, "\\s+", "_")
+  nobad <- stringr::str_replace_all(nospace, "[^_A-Za-z1-9]", "_")
+  return(nobad)
+}
+
+setcleannames <- function(dat){
+  setnames(dat, cleannames(dat))
 }
 
 
